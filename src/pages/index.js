31a298b37320c61512d79ccfb47fd1a3e7f22eab
agtz92 <alfredo.gtz92@gmail.com>
2020-12-01 11:11:42 -0600

    links solved

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ export default function Home({data}) {
     <SimpleSlider/>
     <div style={{marginLeft: "2%", marginRight: "2%"}}>
       <Link to="/categorias/"><Heading color="dark" >Encuentra temas resumidos de: </Heading></Link>
-      <Link to="/"><h2 className="heading-categoria" style={{color:"black"}}>Ve todo en Español {">"}</h2></Link>
+      <Link to="/categorias/espanol/"><h2 className="heading-categoria" style={{color:"black"}}>Ve todo en Español {">"}</h2></Link>
       <Blockcontainer>
         {data.allMarkdownRemark.edges.map(({ node }) => {
           if(node.frontmatter.categoria.includes('Español')){ return(
@@ -32,7 +32,7 @@ export default function Home({data}) {
           }
         })}
       </Blockcontainer>
-      <Link to="/"><h2 className="heading-categoria" style={{color:"black"}}>Ve todo en Literatura {">"}</h2></Link>
+      <Link to="/categorias/literatura/"><h2 className="heading-categoria" style={{color:"black"}}>Ve todo en Literatura {">"}</h2></Link>
       <Blockcontainer>
         {data.allMarkdownRemark.edges.map(({ node }) => {
           if (node.frontmatter.categoria.includes('Literatura')) {
@@ -46,7 +46,7 @@ export default function Home({data}) {
           }
         })}
       </Blockcontainer>
-      <Link to="/"><h2 className="heading-categoria" style={{color:"black"}}>Ve todo en Geografía {">"}</h2></Link>
+      <Link to="/categorias/geografia/"><h2 className="heading-categoria" style={{color:"black"}}>Ve todo en Geografía {">"}</h2></Link>
       <Blockcontainer>
         {data.allMarkdownRemark.edges.map(({ node }) => {
           if(node.frontmatter.categoria.includes('Geografía')){ return(
@@ -59,10 +59,10 @@ export default function Home({data}) {
           }
         })}
       </Blockcontainer>
-      <Link to="/"><h2 className="heading-categoria" style={{color:"black"}}>Ve todo en Historia Universal {">"}</h2></Link>
+      <Link to="/categorias/historia/"><h2 className="heading-categoria" style={{color:"black"}}>Ve todo en Historia Universal {">"}</h2></Link>
       <Blockcontainer>
         {data.allMarkdownRemark.edges.map(({ node }) => {
-          if (node.frontmatter.categoria.includes('Historia')) {
+          if (node.frontmatter.categoria.includes('Historia') && !node.frontmatter.categoria.includes('México')) {
             return (
               <Link key={node.id} to={node.frontmatter.slug}>
                 <PostBlock name={node.frontmatter.title} text={node.frontmatter.short_description} nivel={node.frontmatter.dificultad} background={node.frontmatter.featuredimage} />
@@ -73,10 +73,10 @@ export default function Home({data}) {
           }
         })}
       </Blockcontainer>
-      <Link to="/"><h2 className="heading-categoria" style={{color:"black"}}>Ve todo en Historia de México {">"}</h2></Link>
+      <Link to="/categorias/historia-de-mexico/"><h2 className="heading-categoria" style={{color:"black"}}>Ve todo en Historia de México {">"}</h2></Link>
       <Blockcontainer>
         {data.allMarkdownRemark.edges.map(({ node }) => {
-          if (node.frontmatter.categoria.includes('Historia de México')) {
+          if (node.frontmatter.categoria.includes('México')) {
             return (
               <Link key={node.id} to={node.frontmatter.slug}>
                 <PostBlock name={node.frontmatter.title} text={node.frontmatter.short_description} nivel={node.frontmatter.dificultad} background={node.frontmatter.featuredimage} />
@@ -87,7 +87,7 @@ export default function Home({data}) {
           }
         })}
       </Blockcontainer>
-      <Link to="/"><h2 className="heading-categoria" style={{color:"black"}}>Ve todo en Biología {">"}</h2></Link>
+      <Link to="/categorias/biologia/"><h2 className="heading-categoria" style={{color:"black"}}>Ve todo en Biología {">"}</h2></Link>
       <Blockcontainer>
         {data.allMarkdownRemark.edges.map(({ node }) => {
           if (node.frontmatter.categoria.includes('Biología')) {
