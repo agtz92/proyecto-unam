3c1674e339a2c6a25d15164f4f30afb0043b1e6b
agtz92 <alfredo.gtz92@gmail.com>
2020-12-09 23:19:28 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,16 @@ module.exports = {
     description: `Estudia antes de tu examen de Espanol, Geografia, Literatura, Biologia, Historia y mas!`,
   },
   plugins: [
+     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "G-0529X1RN3D",
+        // this option places the tracking script into the head of the DOM
+        head: true,
+        defer: true,
+        // other options
+      },
+    },
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     `gatsby-plugin-react-helmet`,
@@ -17,35 +27,6 @@ module.exports = {
         sitemap: 'https://www.antesdelexamen.com/sitemap.xml',
         policy: [{ userAgent: '*', allow: '/' }]
       }
-    },
-    {
-    resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "G-0529X1RN3D",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        // anonymize: true,
-        // Setting this parameter is also optional
-        // respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        // exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Defers execution of google analytics script after page load
-        defer: true,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "antesdelexamen.com",
-      },
     },
     {
       resolve: `gatsby-transformer-remark`,
