a3b75a7daaa52bd08a3a5aa624b7cfb0c6d3c0e0
agtz92 <alfredo.gtz92@gmail.com>
2020-12-09 23:53:49 -0600

    ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,5 @@
 
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+
 module.exports = {
   siteMetadata: {
     title: `Antes del Examen | Temas resumidos para cualquier materia`,
@@ -10,13 +8,30 @@ module.exports = {
   },
   plugins: [
      {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: "G-0529X1RN3D",
-        // this option places the tracking script into the head of the DOM
-        head: true,
-        defer: true,
-        // other options
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-0529X1RN3D", // Google Analytics / GA
+          //"AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          //"DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        /*gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },*/
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          // exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
       },
     },
     "gatsby-plugin-sharp",
