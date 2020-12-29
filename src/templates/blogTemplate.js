811f82d8c86a99ad2e3ba1c2d22a98b29c58a3f8
agtz92 <alfredo.gtz92@gmail.com>
2020-12-28 18:23:42 -0600

    Update blogTemplate.js

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -8,6 +8,9 @@ import logoChico from "../images/logo_small.png"
 import '../styles/normalize.css'
 import "../styles/webflow.css"
 import "../styles/soynuevo.webflow.css"
+import matmarkt from '../images/partners/matmarkt.jpg'
+import corthw from '../images/partners/corthw.jpg'
+import InfoBlock from "../components/infoblock"
 // Utilities
 import kebabCase from "lodash/kebabCase"
 import Img from "gatsby-image"
@@ -89,6 +92,16 @@ export default function Template({
                 <div className="div-grey-post"></div>
                 
             </div>
+            <Heading color="dark" alignment="center">Te podría interesar</Heading>
+            <li>
+                <a href="https://www.matmarkt.com/"  rel="noopener norefferer">
+                    <InfoBlock title="MatMarkt" img={matmarkt} description="Mat Markt es una empresa dedicada a la fabricación, distribución y venta de tapetes de hule para uso industrial, comercial y residencial. ¡Da click para visitar el sitio web!"/>
+                </a>
+                <a href="https://www.corthw.com/"  rel="noopener norefferer">
+                    <InfoBlock title="Cortina Hawaiana Mx" img={corthw} description="Tiras de PVC armadas a la medida para protección del polvo, áreas de enfriamiento, separación de áreas, ahuyentar insectos, evitar chispas y todos los usos que necesites aquí los puedes encontrar! ¡Da click para visitar el sitio web!"/>
+                </a>
+                
+            </li>
             </Layout>
             </React.Fragment>
         )
