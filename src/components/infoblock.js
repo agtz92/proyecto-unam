3a505f45abde9d032eabe812a78e868fc9ce906e
agtz92 <alfredo.gtz92@gmail.com>
2020-12-29 19:44:54 -0600

    changes

--- src/components/infoblock.js
+++ src/components/infoblock.js
@@ -5,12 +5,15 @@ import "../styles/webflow.css"
 import "../styles/soynuevo.webflow.css"
 //import Img from "gatsby-image"
 
+function fix_image_path(image_path){
+    return image_path.startsWith("../static/assets/") ? image_path.slice(17) : image_path
+}
 
 export default function InfoBlock(props) {
     return(
         <div className="div-large-post">
             <div className="div-img-large-post">
-            <img src={props.img} loading="lazy" width="250" alt="" className="img-large-post"/>
+            <img src={fix_image_path(props.img)} loading="lazy" width="250" alt="" className="img-large-post"/>
             </div>
                 <div className="div-large-post-content">
                 <Heading color="dark">{props.title}</Heading> 
