b51e1397da6c221252e0004993c38b124cc38cbc
agtz92 <alfredo.gtz92@gmail.com>
2021-01-11 11:27:29 -0600

    Update categorias.js

--- src/templates/categorias.js
+++ src/templates/categorias.js
@@ -2,8 +2,12 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import PropTypes from "prop-types"
 import Layout from "../layouts/layout"
+import Heading from "../components/heading"
 import PostBlockLarge from "../components/postblocklarge"
+import InfoBlock from "../components/infoblock"
 import CategoryHeader from "../components/categoryHeader"
+import matmarkt from '../images/partners/matmarkt.jpg'
+import corthw from '../images/partners/corthw.jpg'
 
 
 // Components
@@ -64,6 +68,16 @@ const Categorias = ({ pageContext, data }) => {
               This links to a page that does not yet exist.
               You'll come back to it!
             */} 
+             <Heading color="dark" alignment="center">Te podría interesar</Heading>
+            <li>
+                <a href="https://www.matmarkt.com/productos"  rel="noopener ">
+                    <InfoBlock title="MatMarkt" img={matmarkt} description="Mat Markt es una empresa dedicada a la fabricación, distribución y venta de tapetes de hule para uso industrial, comercial y residencial. ¡Da click para visitar el sitio web!"/>
+                </a>
+                <a href="https://www.corthw.com/productos"  rel="noopener ">
+                    <InfoBlock title="Cortina Hawaiana Mx" img={corthw} description="Tiras de PVC armadas a la medida para protección del polvo, áreas de enfriamiento, separación de áreas, ahuyentar insectos, evitar chispas y todos los usos que necesites aquí los puedes encontrar! ¡Da click para visitar el sitio web!"/>
+                </a>
+                
+            </li>
             <Link to="/categorias" ><div className="div-tag marginbottom">Ve todas las categorias</div></Link>
         </div>
         </Layout>
