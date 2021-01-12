19ed3a3a23f70164ae6b9c04ec2f849b6e779cab
agtz92 <alfredo.gtz92@gmail.com>
2021-01-11 19:37:31 -0600

    Update tags.js

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -3,6 +3,10 @@ import { Helmet } from "react-helmet"
 import PropTypes from "prop-types"
 import Layout from "../layouts/layout"
 import PostBlockLarge from "../components/postblocklarge"
+import Heading from "../components/heading"
+import InfoBlock from "../components/infoblock"
+import matmarkt from '../images/partners/matmarkt.jpg'
+import corthw from '../images/partners/corthw.jpg'
 
 // Components
 import { Link, graphql } from "gatsby"
@@ -50,6 +54,26 @@ const Tags = ({ pageContext, data }) => {
               You'll come back to it!
             */}
             <Link to="/tags" ><div className="div-tag marginbottom">Ve todos los tags</div></Link>
+
+            <div style={{padding:"10px"}}>
+                <p>En esta página, podrás encontrar todos los temas que tenemos relacionados con la palabra clave {tag}.
+             Todos nuestros resumenes de examen y bancos de preguntas para examen son ideales para estudiar antes de tu examen. 
+             Ya sea para prepararte para la universidad o en preparatoria.
+            Si la etiqueta {tag} no es lo que estás buscando, te recomendamos que busques dentro de nuestras 
+            <Link to="/categorias"> categorías</Link> y busques dentro de nuestro contenido algo que sea relevante para ti. También te invitamos a usar nuestro <Link to="/search">buscador </Link> 
+            donde podrás acceder de una manera mucho más rápida a nuestro contenido.</p>
+            </div>
+            <Heading color="dark" alignment="center">Te podría interesar</Heading>
+            <li>
+                <a href="https://www.matmarkt.com/productos"  rel="noopener ">
+                    <InfoBlock title="MatMarkt" img={matmarkt} description="Mat Markt es una empresa dedicada a la fabricación, distribución y venta de tapetes de hule para uso industrial, comercial y residencial. ¡Da click para visitar el sitio web!"/>
+                </a>
+                <a href="https://www.corthw.com/productos"  rel="noopener ">
+                    <InfoBlock title="Cortina Hawaiana Mx" img={corthw} description="Tiras de PVC armadas a la medida para protección del polvo, áreas de enfriamiento, separación de áreas, ahuyentar insectos, evitar chispas y todos los usos que necesites aquí los puedes encontrar! ¡Da click para visitar el sitio web!"/>
+                </a>
+                
+            </li>
+            
         </div>
         </Layout>
         </React.Fragment>
