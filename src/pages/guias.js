5fcc6b8fd19282de18ccebe108450b732703e599
agtz92 <alfredo.gtz92@gmail.com>
2021-01-25 11:17:24 -0600

    ipn

--- src/pages/guias.js
+++ src/pages/guias.js
@@ -5,6 +5,7 @@ import Heading from "../components/heading"
 import PostBlockLarge from "../components/postblocklarge"
 import Layout from "../layouts/layout"
 import unam from "../images/UNAM.jpg"
+import ipn from "../images/IPN.jpg"
 import '../styles/normalize.css'
 import "../styles/webflow.css"
 import "../styles/soynuevo.webflow.css"
@@ -22,16 +23,20 @@ export default function Home() {
     <div style={{marginLeft: "2%", marginRight: "2%"}}>
         <Heading color="dark" alignment="center">Guías para Universidades 2021</Heading>
         <li>
+            <Link to={`/guia-ipn/`}>
+                <PostBlockLarge title="Guia IPN 2021" img={ipn} nivel="Universidad" description="Guía de estudio IPN 2021. Guía más reciente para admisión al IPN."/>
+            </Link>
+
             <Link to={`/guiaarea1unam/`}>
                 <PostBlockLarge title="Guia Area 1 UNAM" img={unam} nivel="Universidad" description="Guía UNAM Area 1 2021. Guía más reciente para Area 1 admisión de la UNAM."/>
             </Link>
-            <Link to={`/guiaarea1unam/`}>
+            <Link to={`/guiaarea2unam/`}>
                 <PostBlockLarge title="Guia Area 2 UNAM" img={unam} nivel="Universidad" description="Guía UNAM Area 2 2021. Guía más reciente para Area 2 admisión de la UNAM."/>
             </Link>
-            <Link to={`/guiaarea1unam/`}>
+            <Link to={`/guiaarea3unam/`}>
                 <PostBlockLarge title="Guia Area 3 UNAM" img={unam} nivel="Universidad" description="Guía UNAM Area 3 2021. Guía más reciente para Area 3 admisión de la UNAM."/>
             </Link>
-            <Link to={`/guiaarea1unam/`}>
+            <Link to={`/guiaarea4unam/`}>
                 <PostBlockLarge title="Guia Area 4 UNAM" img={unam} nivel="Universidad" description="Guía UNAM Area 4 2021. Guía más reciente para Area 4 admisión de la UNAM."/>
             </Link>
         </li>
