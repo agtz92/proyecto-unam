e2f9896eb3e7b649cd0c8f04ce4ae351120f972c
agtz92 <alfredo.gtz92@gmail.com>
2021-10-18 11:23:28 -0500

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -42,10 +42,11 @@ export default function Footer() {
                     <div>
                     <h2 className="footer-heading">Partners</h2>
                     <ul  className="list-footer w-list-unstyled">
-                        <li><a className="footer-link" href="https://www.matmarkt.com/productos"  rel="noopener noreferrer">MatMarkt. Piso y tapetes de hule y PVC</a></li>
-                        <li><a className="footer-link" href="https://www.corthw.com/productos"  rel="noopener noreferrer">Cortina Hawaiana. Cortinas Hawaianas de PVC</a></li>
-                        <li><a className="footer-link" href="https://www.10datos.com"  rel="noopener noreferrer">10 datos curiosos sobre cientos de temas</a></li>
-                        <li><a className="footer-link" href="https://www.antesdelauni.com"  rel="noopener noreferrer">Bancos de preguntas UNAM IPN UAM UAEM</a></li>
+                        <li><a className="footer-link" href="https://www.matmarkt.com/productos"  rel="noopener ">MatMarkt. Piso y tapetes de hule y PVC</a></li>
+                        <li><a className="footer-link" href="https://www.corthw.com/productos"  rel="noopener ">Cortina Hawaiana. Cortinas Hawaianas de PVC</a></li>
+                        <li><a className="footer-link" href="https://www.10datos.com"  rel="noopener ">10 datos curiosos sobre cientos de temas</a></li>
+                        <li><a className="footer-link" href="https://www.antesdelauni.com"  rel="noopener ">Bancos de preguntas UNAM IPN UAM UAEM</a></li>
+                        <li><a className="footer-link" href="https://www.3minread.com"  rel="noopener ">Lifestyle, Culture and more 3 min read articles</a></li>
                     </ul>
                     </div>
                 </div>
