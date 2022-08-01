747e85d5c8be9d8fdbc93e6459d5576ac69954be
agtz92 <alfredo.gtz92@gmail.com>
2022-08-01 10:17:45 -0500

    slick update

--- src/components/heading.js
+++ src/components/heading.js
@@ -2,8 +2,10 @@ import React from "react"
 import '../styles/normalize.css'
 import "../styles/webflow.css"
 import "../styles/soynuevo.webflow.css"
+import "../webflow_styles/symbols/heading.css"
 
 
 export default function Heading(props) {
-    return <h1 className={'heading ' + props.color + " " + props.alignment + " " + props.size}>{props.children}</h1>
+    /*return <h1 className={'heading ' + props.color + " " + props.alignment + " " + props.size}>{props.children}</h1>*/
+    return <h1 className={`${props.alignment} ${" "} ${props.color} ${" "} ${props.extraclass} ${" "} ${props.size}`}>{props.children}</h1>;
 }
