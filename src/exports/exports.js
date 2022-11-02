5528c48fe7769f0630e8b0548e1d8ca06f5e30d9
agtz92 <alfredo.gtz92@gmail.com>
2022-11-01 21:09:10 -0600

    Update exports.js

--- src/exports/exports.js
+++ src/exports/exports.js
@@ -1,20 +1,21 @@
 //Categorías con la imagen
 
 exports.array = {
-    Español: require("../images/slider/espanol.jpg"),
-    Literatura: require("../images/slider/literatura.jpg"),
-    Geografía: require("../images/slider/geografia.jpg"),
-    HistoriaUniversal: require("../images/slider/historia.jpg"),
-    HistoriadeMéxico: require("../images/slider/historiamexico.jpg"),
-    Biología: require("../images/slider/biologia.jpg"),
-    PreguntasdeExamen: require("../images/slider/preguntasdeexamen.jpg"),
-    Química: require("../images/slider/quimica.jpg"),
-    Contabilidad: require("../images/slider/contabilidad.jpg"),
-    ContabilidadDeCostos: require("../images/slider/contabilidaddecostos.jpg"),
-    Mercadotencia: require("../images/slider/mercadotecnia.jpg"),
-    Emprendedurismo: require("../images/slider/emprendedurismo.jpg"),
-    Economia: require("../images/slider/economia.jpg"),
-    Derecho: require("../images/slider/derecho.jpg"),
-    MejorescursosdepreparacionUNAMIPNUAMUAEM: require("../images/slider/cursosdepreparacion.jpg"),
-    
-  }
+  Español: require("../images/slider/espanol.jpg"),
+  Literatura: require("../images/slider/literatura.jpg"),
+  Geografía: require("../images/slider/geografia.jpg"),
+  HistoriaUniversal: require("../images/slider/historia.jpg"),
+  HistoriadeMéxico: require("../images/slider/historiamexico.jpg"),
+  Biología: require("../images/slider/biologia.jpg"),
+  PreguntasdeExamen: require("../images/slider/preguntasdeexamen.jpg"),
+  Química: require("../images/slider/quimica.jpg"),
+  Contabilidad: require("../images/slider/contabilidad.jpg"),
+  ContabilidadDeCostos: require("../images/slider/contabilidaddecostos.jpg"),
+  Mercadotencia: require("../images/slider/mercadotecnia.jpg"),
+  Emprendedurismo: require("../images/slider/emprendedurismo.jpg"),
+  Economia: require("../images/slider/economia.jpg"),
+  Derecho: require("../images/slider/derecho.jpg"),
+  MejorescursosdepreparacionUNAMIPNUAMUAEM: require("../images/slider/cursosdepreparacion.jpg"),
+  SistemasDeInformacionContableYFinanciera: require("../images/slider/sistemasdeinformacioncontableyfinanciera.jpg"),
+  
+}
